ad64dad7bc0a2715b4fe14a5ff1ba2b57dec9c5d
josuhun <chosh9128@gmail.com>
2024-01-16 23:08:35 +0900

    [feat]Main에 Navbar 추가함

--- pwa-react/src/routes/Main.tsx
+++ pwa-react/src/routes/Main.tsx
@@ -1,10 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 
-import { Wrapper, FirstSection, SecondSection } from "../styles/Main/Main";
+import { Wrapper, BurgerButton, FirstSection, SecondSection } from "../styles/Main/Main";
 
-import { Button } from "../styles/common/button";
-import { Card } from "../styles/common/card";
+import Navbar from "../components/Navbar";
 import HeaderSection from "../components/Main/HeaderSection";
 import CameraCard from "../components/Main/CameraCard";
 import GalleryCard from "../components/Main/GalleryCard";
@@ -14,8 +13,16 @@ import Calendar from "../components/Main/Calendar";
 
 const Main = () => {
   const { id } = useParams();
+  const [isNavigationOpen, setIsNavigationOpen] = useState(false)
+
+  const toggleNavigation = () => {
+    setIsNavigationOpen(!isNavigationOpen);
+  };
+
   return (
     <>
+    <BurgerButton onClick={toggleNavigation}>☰</BurgerButton>
+      <Navbar isOpen={isNavigationOpen} onClose={toggleNavigation} />
       <HeaderSection />
 
       <Wrapper>
