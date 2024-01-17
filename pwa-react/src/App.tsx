4e777c0898580445c7895b07eaa95754d5fb6856
012coding <sugo2753@gmail.com>
2024-01-17 09:26:34 +0900

    [update] App 라우터 설정

--- pwa-react/src/App.tsx
+++ pwa-react/src/App.tsx
@@ -5,15 +5,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Login from "./routes/Login";
 import Main from "./routes/Main";
+import SignUp from "./routes/SignUp";
+import useAuthStore from "./stores/AuthStore";
 
 function App() {
+  const { isLogIn } = useAuthStore();
+
   return (
     <BrowserRouter>
       <div>
         <Global />
         <Routes>
+          <Route path="/" element={ isLogIn ? <Main /> : <Login />} />
+          <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<Login />}></Route>
-          <Route path="/" element={<Main />}/>
+          <Route path="/main" element={<Main />}/>
         </Routes>
       </div>
     </BrowserRouter>
