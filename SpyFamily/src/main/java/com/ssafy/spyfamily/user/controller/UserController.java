1d235519ec01bc444a7873ef3a40ee9b77c11bda
이기영 <rldud112@naver.com>
2024-01-17 11:56:28 +0900

    "fix social login"

--- SpyFamily/src/main/java/com/ssafy/spyfamily/user/controller/UserController.java
+++ SpyFamily/src/main/java/com/ssafy/spyfamily/user/controller/UserController.java
@@ -1,30 +1,33 @@
 package com.ssafy.spyfamily.user.controller;
 
-import com.ssafy.spyfamily.user.model.KakaoUser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ssafy.spyfamily.user.model.UserInfo;
-import com.ssafy.spyfamily.user.service.GoogleApiService;
+import com.ssafy.spyfamily.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.client.RestTemplate;
+import java.util.Base64;
+import java.util.Map;
 
 @RestController
-@CrossOrigin(origins = { "*" }, methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE} , maxAge = 6000)
+@RequestMapping(value = "/login/oauth2", produces = "application/json")
+@CrossOrigin(origins = "http://localhost:3000", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE}, maxAge = 6000)
 public class UserController {
 
-    private final GoogleApiService googleApiService;
+    private final UserService userService;
 
-    public UserController(GoogleApiService googleApiService) {
-        this.googleApiService = googleApiService;
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
-    @PostMapping("/login/oauth2/code/google")
-    public ResponseEntity<UserInfo> googleLoginPost(@RequestParam(name = "accessToken") String accessToken) {
+    @PostMapping("/code/google")
+    public ResponseEntity<UserInfo> googleLoginPost(@RequestParam String access_Token) {
         try {
             // Google API로부터 사용자 정보 얻기
-            UserInfo userInfo = googleApiService.getUserInfo(accessToken);
+            UserInfo userInfo = userService.getGoogleUserInfo(access_Token);
 
             // 사용자 정보 출력 예시
             System.out.println("User ID: " + userInfo.getUserId());
@@ -39,38 +42,19 @@ public class UserController {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-
-    @PostMapping("/login/oauth2/code/kakao")
-    public ResponseEntity<?> kakaoLoginPost(@RequestParam(name = "accessToken") String accessToken) {
-        // Kakao API 요청을 위한 정보
-        String kakaoTokenUrl = "https://kauth.kakao.com/oauth/token";
-        String grantType = "authorization_code";
-        String clientId = "5a8a53240d6799ecf38d7454ab5579b3";
-        String redirectUri = "http://localhost:3000";
-
-        // 헤더 설정
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-
-        // 파라미터 설정
-        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        params.add("grant_type", grantType);
-        params.add("client_id", clientId);
-        params.add("redirect_uri", redirectUri);
-        params.add("code", accessToken);
-
-        // 요청 생성
-        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(params, headers);
+    @PostMapping("/code/kakao")
+    public ResponseEntity<?> kakaoLoginPost(@RequestParam String code) throws JsonProcessingException {
 
         // Kakao API에 POST 요청
-        ResponseEntity<KakaoUser> responseEntity = new RestTemplate().postForEntity(kakaoTokenUrl, request, KakaoUser.class);
+        ResponseEntity<UserInfo> responseEntity = userService.getKakaoPost(code);
 
         // HTTP 상태 코드 확인
-        HttpStatus statusCode = (HttpStatus) responseEntity.getStatusCode();
+        HttpStatusCode statusCode = responseEntity.getStatusCode();
         System.out.println("HTTP Status Code: " + statusCode);
 
+
         if (statusCode == HttpStatus.OK) {
-            KakaoUser kakaoUser = responseEntity.getBody();
+            UserInfo kakaoUser = responseEntity.getBody();
 
             System.out.println("Received KakaoUser:");
             System.out.println("Token Type: " + kakaoUser.getToken_type());
@@ -80,25 +64,28 @@ public class UserController {
             System.out.println("Refresh Token: " + kakaoUser.getRefresh_token());
             System.out.println("Refresh Token Expires In: " + kakaoUser.getRefresh_token_expires_in());
 
-//            // JWT 해석
-//            Jws<Claims> claimsJws = Jwts.parser().setSigningKey("your-signing-key").parseClaimsJws(kakaoUser.getId_token());
-//            Claims claims = claimsJws.getBody();
-//
-//            // JWT에서 필요한 정보 추출
-//            String email = claims.get("email", String.class);
-//            String nickname = claims.get("nickname", String.class);
-//            String picture = claims.get("picture", String.class);
-//
-//            System.out.println();
+
+            String token = kakaoUser.getId_token();
+            String[] check = token.split("\\.");
+            Base64.Decoder decoder = Base64.getDecoder();
+            String payload = new String(decoder.decode(check[1]));
+
+            ObjectMapper mapper = new ObjectMapper();
+            Map<String, Object> returnMap = mapper.readValue(payload, Map.class);
+
+            System.out.println();
 
             // UserInfo 객체에 담기
-//            UserInfo userInfo = new UserInfo();
-//            userInfo.setEmail(email);
-//            userInfo.setNickname(nickname);
-//            userInfo.setPicture(picture);
+            UserInfo userInfo = new UserInfo();
+            userInfo.setEmail((String) returnMap.get("email"));
+            userInfo.setNickname((String) returnMap.get("nickname"));
+            userInfo.setPicture((String) returnMap.get("picture"));
 
-//            return new ResponseEntity<>(userInfo, HttpStatus.OK);
-            return new ResponseEntity<Void>(HttpStatus.OK);
+            System.out.println(userInfo.getNickname());
+            System.out.println(userInfo.getEmail());
+            System.out.println(userInfo.getPicture());
+
+            return new ResponseEntity<>(userInfo, HttpStatus.OK);
         } else {
             // 에러 처리 등을 수행
             System.out.println("Error response: " + responseEntity.getBody());
@@ -107,6 +94,45 @@ public class UserController {
 
         // 결과 반환
 //        return responseEntity;
+    }
+
+    @RequestMapping(value = "/code/naver")
+    public ResponseEntity<?> naverLogin(@RequestParam String code, @RequestParam String state) {
+
+        // naver API에 POST 요청
+        ResponseEntity<UserInfo> responseEntity = userService.getNaverPost(code, state);
+
+        // HTTP 상태 코드 확인
+        HttpStatusCode statusCode = responseEntity.getStatusCode();
+        System.out.println("HTTP Status Code: " + statusCode);
+
+        if (statusCode == HttpStatus.OK) {
+            UserInfo naverUser = responseEntity.getBody();
+
+            System.out.println("Received Naver User:");
+            System.out.println("Token Type: " + naverUser.getToken_type());
+            System.out.println("Access Token: " + naverUser.getAccess_token());
+            System.out.println("Expires In: " + naverUser.getExpires_in());
+            System.out.println("Refresh Token: " + naverUser.getRefresh_token());
+
+
+            UserInfo userInfo = userService.getNaverUserInfo(naverUser.getAccess_token());
+
+            System.out.println("네이버 로그인");
+            String jsonString = userService.convertObjectToJson(userInfo.getResponse());
+            Map<String, String> dataMap = userService.convertJsonToMap(jsonString);
+
+            for (Map.Entry<String, String> entry : dataMap.entrySet()) {
+                System.out.println(entry.getKey() + ": " + entry.getValue());
+            }
+
+            return new ResponseEntity<>(userInfo, HttpStatus.OK);
+        } else {
+            // 에러 처리 등을 수행
+            System.out.println("Error response: " + responseEntity.getBody());
+            return new ResponseEntity<Void>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
     }
 
 }
--- SpyFamily/src/main/java/com/ssafy/spyfamily/user/service/UserService.java
+++ SpyFamily/src/main/java/com/ssafy/spyfamily/user/service/UserService.java
@@ -1,4 +1,182 @@
 package com.ssafy.spyfamily.user.service;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ssafy.spyfamily.user.model.UserInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
 public class UserService {
+
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public UserService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public UserInfo getGoogleUserInfo(String accessToken) {
+        // Google API의 유저 정보를 얻기 위한 엔드포인트
+        String userInfoEndpoint = "https://www.googleapis.com/oauth2/v3/userinfo";
+
+        // HTTP 헤더 설정
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + accessToken);
+
+        // HTTP 요청 생성
+        HttpEntity entity = new HttpEntity(headers);
+
+        // Google API에 GET 요청을 보내고 응답을 UserInfo 클래스로 매핑
+        ResponseEntity<UserInfo> response = restTemplate.exchange(
+                userInfoEndpoint,
+                HttpMethod.GET,
+                entity,
+                UserInfo.class
+        );
+
+        // 응답 결과 반환
+        if (response.getStatusCode() == HttpStatus.OK) {
+            return response.getBody();
+        } else {
+            // 오류 처리
+            throw new RuntimeException("Failed to retrieve user information from Google API");
+        }
+    }
+
+
+    public UserInfo getNaverUserInfo(String accessToken) {
+        // Google API의 유저 정보를 얻기 위한 엔드포인트
+        String userInfoEndpoint = "https://openapi.naver.com/v1/nid/me";
+
+        // HTTP 헤더 설정
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + accessToken);
+
+        // HTTP 요청 생성
+        HttpEntity entity = new HttpEntity(headers);
+
+        // Google API에 GET 요청을 보내고 응답을 UserInfo 클래스로 매핑
+        ResponseEntity<UserInfo> response = restTemplate.exchange(
+                userInfoEndpoint,
+                HttpMethod.GET,
+                entity,
+                UserInfo.class
+        );
+
+        // 응답 결과 반환
+        if (response.getStatusCode() == HttpStatus.OK) {
+            return response.getBody();
+        } else {
+            // 오류 처리
+            throw new RuntimeException("Failed to retrieve user information from Google API");
+        }
+    }
+
+    public ResponseEntity<UserInfo> getKakaoPost(String code) {
+        // Kakao API 요청을 위한 정보
+        String kakaoTokenUrl = "https://kauth.kakao.com/oauth/token";
+        String grantType = "authorization_code";
+        String clientId = "5a8a53240d6799ecf38d7454ab5579b3";
+        String redirectUri = "http://localhost:3000";
+
+        // 헤더 설정
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        // 파라미터 설정
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", grantType);
+        params.add("client_id", clientId);
+        params.add("redirect_uri", redirectUri);
+        params.add("code", code);
+
+        // 요청 생성
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(params, headers);
+
+        // Kakao API에 POST 요청
+        ResponseEntity<UserInfo> responseEntity = new RestTemplate().postForEntity(kakaoTokenUrl, request, UserInfo.class);
+        return responseEntity;
+    }
+
+    public ResponseEntity<UserInfo> getNaverPost(String code, String state) {
+        String naver_url = "https://nid.naver.com/oauth2.0/token";
+        String clientId = "H3hbCg2IUznqo9J6IdEO";
+        String Client_Secret = "YnfC2lwoGL";
+
+        // 헤더 설정
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        // 파라미터 설정
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("client_id", clientId);
+        params.add("client_secret", Client_Secret);
+        params.add("grant_type", "authorization_code");
+        params.add("state", state);
+        params.add("code", code);
+
+        // 요청 생성
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(params, headers);
+
+        // naver API에 POST 요청
+        ResponseEntity<UserInfo> responseEntity = new RestTemplate().postForEntity(naver_url, request, UserInfo.class);
+
+        return responseEntity;
+    }
+
+    public static Map<String, String> convertJsonToMap(String jsonString) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, String> dataMap = new HashMap<>();
+
+        try {
+            // JSON 문자열을 JsonNode로 매핑
+            JsonNode rootNode = objectMapper.readTree(jsonString);
+
+            // response 아래의 필드를 Map에 담기
+            rootNode.fields().forEachRemaining(entry -> {
+                dataMap.put(entry.getKey(), entry.getValue().asText());
+            });
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return dataMap;
+    }
+
+
+    public static String convertObjectToJson(Object object) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            // response 필드를 따로 JSON 문자열로 변환
+            if (object instanceof UserInfo) {
+                UserInfo userInfo = (UserInfo) object;
+                Object response = userInfo.getResponse();
+                if (response != null) {
+                    String responseJson = objectMapper.writeValueAsString(response);
+                    // response를 JSON 문자열로 변환하여 Map에 추가
+                    Map<String, Object> dataMap = objectMapper.readValue(responseJson, new TypeReference<Map<String, Object>>() {});
+                    Map<String, Object> resultMap = new HashMap<>();
+                    resultMap.putAll(dataMap);
+                    resultMap.put("response", responseJson);
+                    return objectMapper.writeValueAsString(resultMap);
+                }
+            }
+            // 그 외의 경우는 전체 객체를 JSON으로 변환
+            return objectMapper.writeValueAsString(object);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 }
