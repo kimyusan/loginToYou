d931e7a88297a4facb77c70eb2d125781ab4d067
김유산 <rladbtks@naver.com>
2024-01-24 17:55:08 +0900

    [update] chatting loadmessage

--- SpyFamily/src/main/java/com/ssafy/spyfamily/chat/repo/ChatRoomRepo.java
+++ SpyFamily/src/main/java/com/ssafy/spyfamily/chat/repo/ChatRoomRepo.java
@@ -3,7 +3,11 @@ package com.ssafy.spyfamily.chat.repo;
 import com.ssafy.spyfamily.chat.model.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ChatRoomRepo extends JpaRepository<ChatRoom, String> {
 
     ChatRoom findByCoupleId(Integer coupleId);
+
+
 }
