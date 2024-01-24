c286fb5f7697134e17ad1616ad3ca4366d24d925
김영일 <sugo2753@gmail.com>
2024-01-24 10:38:52 +0900

    [fix] 캘린더 삭제 파라미터 이름 추가

--- SpyFamily/src/main/java/com/ssafy/spyfamily/calendar/controller/CalendarController.java
+++ SpyFamily/src/main/java/com/ssafy/spyfamily/calendar/controller/CalendarController.java
@@ -75,7 +75,7 @@ public class CalendarController {
     }
 
     @DeleteMapping("/delete")
-    public ResponseEntity<?> deleteCalendar(@RequestParam Integer calender_id) {
+    public ResponseEntity<?> deleteCalendar(@RequestParam(name="calender_id") Integer calender_id) {
         try {
             // 삭제 로직 수행
             calendarService.deleteCalendar(calender_id);
