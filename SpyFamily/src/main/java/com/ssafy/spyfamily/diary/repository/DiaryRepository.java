b1f08b4c8a1f83e573f746142139b261ec8e6783
이기영 <rldud112@naver.com>
2024-02-01 17:15:08 +0900

    [fix] 다이어리 썸네일 관련 수정

--- SpyFamily/src/main/java/com/ssafy/spyfamily/diary/repository/DiaryRepository.java
+++ SpyFamily/src/main/java/com/ssafy/spyfamily/diary/repository/DiaryRepository.java
@@ -2,6 +2,7 @@ package com.ssafy.spyfamily.diary.repository;
 
 import com.ssafy.spyfamily.diary.model.Diary;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -11,4 +12,8 @@ public interface DiaryRepository extends JpaRepository<Diary, Integer> {
     ArrayList<Diary> findByCoupleId(Integer coupleId);
 
     Integer countByRegisterDate(String registerDate);
+
+    @Query("SELECT d FROM Diary  d WHERE d.coupleId = :coupleId AND d.registerDate = :registerDate")
+    ArrayList<Diary> findByCoupleIdAndRegisterDate(Integer coupleId, String registerDate);
+
 }
